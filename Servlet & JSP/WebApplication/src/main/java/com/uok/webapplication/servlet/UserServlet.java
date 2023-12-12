d6ab64f78b4d44ff8ecd27c0aaf0646d77a996d7
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2023-12-12 23:47:47 +0530

    Save as a json

--- Servlet & JSP/WebApplication/src/main/java/com/uok/webapplication/servlet/UserServlet.java
+++ Servlet & JSP/WebApplication/src/main/java/com/uok/webapplication/servlet/UserServlet.java
@@ -1,11 +1,14 @@
 package com.uok.webapplication.servlet;
 
+import com.google.gson.Gson;
 import com.uok.webapplication.model.User;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -29,6 +32,21 @@ public class UserServlet extends HttpServlet {
         String username = request.getParameter("username");
         user.setUsername(username);
         pw.println("Hello " + user.getUsername() + " from post");
+        System.out.println(new Gson().toJson(user));
+
+        // Create an instance of Gson
+        Gson gson = new Gson();
+
+        // Convert the object to a JSON string
+        String json = gson.toJson(user);
+
+        // Create a FileWriter object
+        FileWriter writer = new FileWriter("users.json");
+
+        // Write the JSON string to the file
+        writer.write(json);
+        // Close the writer
+        writer.close();
     }
 
 }
